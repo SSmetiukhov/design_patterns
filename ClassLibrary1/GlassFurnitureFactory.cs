b9d778b553badd1d5c4040a481583cc877474334
student <student@K05-09.lan.itstep.kiev.ua>
2019-11-21 18:03:03 +0200

    реализация классов фабрик

--- ClassLibrary1/GlassFurnitureFactory.cs
+++ ClassLibrary1/GlassFurnitureFactory.cs
@@ -7,5 +7,18 @@ namespace ClassLibrary1
 {
     public class GlassFurnitureFactory : AbstractFactory
     {
+        public override AbstractTable CreateTable()
+        {
+            return new GlassTable();
+        }
+        public override AbstractChair CreateChair()
+        {
+            return new GlassChair();
+        }
+        public override AbstractSofa CreateSofa()
+        {
+            return new GlassSofa();
+        }
     }
+
 }
