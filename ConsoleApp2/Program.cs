8dad5c8a13e46043690232878f6bc190c6a1458a
student <student@K05-09.lan.itstep.kiev.ua>
2019-11-21 17:07:53 +0200

    Первое сохранение

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\n\t======================================================");
+            Console.WriteLine("\t\t Система управления мебельным магазином ");
+            Console.WriteLine("\t======================================================\n");
+            Console.WriteLine("\t\t\t Програма завершена");
+
+        }
+    }
+}
